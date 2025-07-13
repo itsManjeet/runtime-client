2f10e535c6504a011ea970a129a9d88ff9870a45
itsmanjeet <itsmanjeet1998@gmail.com>
2025-07-13 22:26:47 +0530

    added control buttons

--- src/runtime-window.c
+++ src/runtime-window.c
@@ -9,17 +9,17 @@ struct _RuntimeWindow
 {
   GtkApplicationWindow parent_instance;
   WebKitWebView *webview;
+  GtkWidget *address_bar;
 };
 
 G_DEFINE_TYPE (RuntimeWindow, runtime_window, GTK_TYPE_APPLICATION_WINDOW)
 
 static void
-on_title_changed (WebKitWebView *webview, GParamSpec *pspec,
-                  gpointer user_data)
+on_uri_changed (WebKitWebView *webview, GParamSpec *pspec, gpointer user_data)
 {
   RuntimeWindow *self = RUNTIME_WINDOW (user_data);
-  const gchar *title = webkit_web_view_get_title (webview);
-  gtk_window_set_title (GTK_WINDOW (self), title ? title : "");
+  const gchar *uri = webkit_web_view_get_uri (self->webview);
+  gtk_entry_set_text (GTK_ENTRY (self->address_bar), uri ? uri : "");
 }
 
 static void
@@ -71,6 +71,35 @@ inject_global_css_js (RuntimeWindow *self)
     }
 }
 
+static void
+go_uri (GtkWidget *widget, gpointer user_data)
+{
+  RuntimeWindow *self = RUNTIME_WINDOW (user_data);
+  const gchar *uri = gtk_entry_get_text (GTK_ENTRY (self->address_bar));
+  webkit_web_view_load_uri (WEBKIT_WEB_VIEW (self->webview), uri);
+}
+
+static void
+go_back (GtkWidget *widget, gpointer user_data)
+{
+  RuntimeWindow *self = RUNTIME_WINDOW (user_data);
+  webkit_web_view_go_back (self->webview);
+}
+
+static void
+go_forward (GtkWidget *widget, gpointer user_data)
+{
+  RuntimeWindow *self = RUNTIME_WINDOW (user_data);
+  webkit_web_view_go_forward (self->webview);
+}
+
+static void
+go_refresh (GtkWidget *widget, gpointer user_data)
+{
+  RuntimeWindow *self = RUNTIME_WINDOW (user_data);
+  webkit_web_view_reload (self->webview);
+}
+
 RuntimeWindow *
 runtime_window_new (GtkApplication *app, const char *uri)
 {
@@ -82,6 +111,32 @@ runtime_window_new (GtkApplication *app, const char *uri)
   GtkWidget *header = gtk_header_bar_new ();
   gtk_header_bar_set_show_close_button (GTK_HEADER_BAR (header), TRUE);
   gtk_window_set_titlebar (GTK_WINDOW (self), header);
+
+  GtkWidget *back_button
+      = gtk_button_new_from_icon_name ("go-previous", GTK_ICON_SIZE_BUTTON);
+  g_signal_connect (back_button, "clicked", G_CALLBACK (go_back), self);
+  gtk_header_bar_pack_start (GTK_HEADER_BAR (header), back_button);
+
+  GtkWidget *next_button
+      = gtk_button_new_from_icon_name ("go-next", GTK_ICON_SIZE_BUTTON);
+  g_signal_connect (next_button, "clicked", G_CALLBACK (go_forward), self);
+  gtk_header_bar_pack_start (GTK_HEADER_BAR (header), next_button);
+
+  GtkWidget *reload_button
+      = gtk_button_new_from_icon_name ("view-refresh", GTK_ICON_SIZE_BUTTON);
+  g_signal_connect (reload_button, "clicked", G_CALLBACK (go_refresh), self);
+  gtk_header_bar_pack_start (GTK_HEADER_BAR (header), reload_button);
+
+  self->address_bar = gtk_entry_new ();
+  gtk_widget_set_hexpand (self->address_bar, TRUE);
+  g_signal_connect (self->address_bar, "activate", G_CALLBACK (go_uri), self);
+  gtk_entry_set_icon_from_icon_name (GTK_ENTRY (self->address_bar),
+                                     GTK_ICON_SIZE_BUTTON, "view-refresh");
+  gtk_header_bar_set_custom_title (GTK_HEADER_BAR (header), self->address_bar);
+
+  GtkWidget *pref
+      = gtk_button_new_from_icon_name ("open-menu", GTK_ICON_SIZE_BUTTON);
+  gtk_header_bar_pack_end (GTK_HEADER_BAR (header), pref);
 
   self->webview = WEBKIT_WEB_VIEW (webkit_web_view_new ());
   gtk_container_add (GTK_CONTAINER (self), GTK_WIDGET (self->webview));
@@ -95,8 +150,8 @@ runtime_window_new (GtkApplication *app, const char *uri)
   WebKitSettings *settings = webkit_web_view_get_settings (self->webview);
   webkit_settings_set_enable_site_specific_quirks (settings, TRUE);
 
-  g_signal_connect (self->webview, "notify::title",
-                    G_CALLBACK (on_title_changed), self);
+  g_signal_connect (self->webview, "notify::uri", G_CALLBACK (on_uri_changed),
+                    self);
   g_signal_connect (self->webview, "notify::favicon",
                     G_CALLBACK (on_favicon_changed), self);
   webkit_settings_set_enable_write_console_messages_to_stdout (settings, TRUE);
